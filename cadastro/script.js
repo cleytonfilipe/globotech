1bae0616cce7de85f5b3cfb2e7969c76071dd23f
Cleyton Filipe <cleyton.sdv@gmail.com>
2023-12-08 23:58:09 -0300

    Site quase pronto

---
+++ cadastro/script.js
@@ -0,0 +1,17 @@
+document.getElementById('cadastroForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    
+
+    alert('Cadastro enviado com sucesso!');
+});
+
+function enviarFeedback() {
+    const feedbackText = document.getElementById('feedback').value;
+
+   
+
+    alert('Feedback enviado com sucesso!');
+
+}
+
---
+++ projetos/main.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll("section");
+
+sections.forEach((section) => {
+    //Add active class to all sections on the page according to the position of the page scrolled so far on page load and scroll
+
+    //Revealing elements one by one on scroll
+
+    //Revealing elements one by one on scroll according to a revealing point
+
+    //Revealing elements one by one on load (page reload)
+});
